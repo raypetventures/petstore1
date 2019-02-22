1feb76bd49b749414ee8bc465180ffd868a82056
raypetventures <47116433+raypetventures@users.noreply.github.com>
2019-02-22 17:25:46 +0000

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,321 @@
+$('body').on('click','#product-info-modalhighenergy',function() {
+  $("#modal-highenergy").slideDown();
+})
+
+$('body').on('click','#product-info-modalhighperformance',function() {
+  $("#modal-highperformance").slideDown();
+})
+
+$('body').on('click','#product-info-modalhighstandardperformance',function() {
+  $("#modal-highstandardperformance").slideDown();
+})
+
+$('body').on('click','#product-info-modalhistandardhienergy24-20',function() {
+  $("#modal-histandardhienergy24-20").slideDown();
+})
+
+$('body').on('click','#product-info-modalhistandardperformance30-20',function() {
+  $("#modal-histandardperformance30-20").slideDown();
+})
+
+$('body').on('click','#product-info-modalhistandardpremiumperformancepuppy',function() {
+  $("#modal-histandardpremiumperformancepuppy").slideDown();
+})
+//------------------------------------ end of dry dog food
+
+// WET DOG FOOD - modal
+$('body').on('click','#product-info-modalpedigreechoppedgrounddinner',function() {
+  $("#modal-pedigreechoppedgrounddinner").slideDown();
+})
+
+$('body').on('click','#product-info-modalalpocanneddogfood',function() {
+  $("#modal-alpocanneddogfood").slideDown();
+})
+
+$('body').on('click','#product-info-modalwinalotcannedfood',function() {
+  $("#modal-winalotcannedfood").slideDown();
+})
+//---------------------------------------end of wet dog food
+
+//Dog treat and dog bone - modal
+$('body').on('click','#product-info-modalchubroll',function() {
+  $("#modal-chubroll").slideDown();
+})
+
+$('body').on('click','#product-info-modalserranohambones',function() {
+  $("#modal-serranohambones").slideDown();
+})
+//--------------------------------end of dog treat and dog
+
+//Dog Vitamins and supplements - modal
+$('body').on('click','#product-info-modalwildalaskansalmonoil',function() {
+  $("#modal-wildalaskansalmonoil").slideDown();
+})
+
+$('body').on('click','#product-info-modalbrewersdriedyeast',function() {
+  $("#modal-brewersdriedyeast").slideDown();
+})
+
+$('body').on('click','#product-info-modalsuregrow100',function() {
+  $("#modal-suregrow100").slideDown();
+})
+
+$('body').on('click','#product-info-modalhappyjackmilkade',function() {
+  $("#modal-happyjackmilkade").slideDown();
+})
+
+$('body').on('click','#product-info-modalprosensedogvitamins',function() {
+  $("#modal-prosensedogvitamins").slideDown();
+})
+//------------------------------end of dogvitamins
+
+
+
+//DOG BEDS AND Baskets
+$('body').on('click','#product-info-modalgreenwaterresistantmattress',function() {
+  $("#modal-greenwaterresistantmattress").slideDown();
+})
+
+$('body').on('click','#product-info-modalgreyjumbocord',function() {
+  $("#modal-greyjumbocord").slideDown();
+})
+
+$('body').on('click','#product-info-modalredorthopaedic',function() {
+  $("#modal-redorthopaedic").slideDown();
+})
+
+//---------------------- end of
+
+//DOG LEADS AND Collars
+$('body').on('click','#product-info-modalropetwistslipleads',function() {
+  $("#modal-ropetwistleads").slideDown();
+})
+
+$('body').on('click','#product-info-modalflexinewcomfort',function() {
+  $("#modal-flexinewcomfort").slideDown();
+})
+
+$('body').on('click','#product-info-modalsoftprotectioncollarsred',function() {
+  $("#modal-softprotectioncollarsred").slideDown();
+})
+
+$('body').on('click','#product-info-modalsoftprotectionharnesses',function() {
+  $("#modal-softprotectionharnesses").slideDown();
+})
+
+$('body').on('click','#product-info-modalreflectiveeasyfitharness',function() {
+  $("#modal-reflectiveeasyfitharness").slideDown();
+})
+//---------------------------------END OF
+
+//DOG TOYS AND Accessories
+$('body').on('click','#product-info-modalplubbersticks',function() {
+  $("#modal-plubbersticks").slideDown();
+})
+$('body').on('click','#product-info-modalplubberdinobone',function() {
+  $("#modal-plubberdinobone").slideDown();
+})
+$('body').on('click','#product-info-modalplubbergrababitegrilledsteak',function() {
+  $("#modal-plubbergrababitegrilledsteak").slideDown();
+})
+
+$('body').on('click','#product-info-modalplubbergrababitelambchop',function() {
+  $("#modal-plubbergrababitelambchop").slideDown();
+})
+$('body').on('click','#product-info-modalrugbyballwithrope',function() {
+  $("#modal-rugbyballwithrope").slideDown();
+})
+$('body').on('click','#product-info-modaltreatball',function() {
+  $("#modal-treatball").slideDown();
+})
+$('body').on('click','#product-info-modaldumbell',function() {
+  $("#modal-dumbell").slideDown();
+})
+$('body').on('click','#product-info-modalplubbergrababitesalmon',function() {
+  $("#modal-plubbergrababitesalmon").slideDown();
+})
+$('body').on('click','#product-info-modaltwistabletugbone',function() {
+  $("#modal-twistabletugbone").slideDown();
+})
+$('body').on('click','#product-info-modalsmallcottonropebone',function() {
+  $("#modal-smallcottonropebone").slideDown();
+})
+
+$('body').on('click','#product-info-modallostsolesneaker',function() {
+  $("#modal-lostsolesneaker").slideDown();
+})
+$('body').on('click','#product-info-modallostsoleworkboot',function() {
+  $("#modal-lostsoleworkboot").slideDown();
+})
+$('body').on('click','#product-info-modalvinylpuppypastelballs',function() {
+  $("#modal-vinylpuppypastelballs").slideDown();
+})
+$('body').on('click','#product-info-modalpuppydumbell',function() {
+  $("#modal-puppydumbell").slideDown();
+})
+$('body').on('click','#product-info-modalrubberandropeball',function() {
+  $("#modal-rubberandropeball").slideDown();
+})
+$('body').on('click','#product-info-modalmeatychickentakeway',function() {
+  $("#modal-meatychickentakeway").slideDown();
+})
+$('body').on('click','#product-info-modaleverlastingtreatball',function() {
+  $("#modal-everlastingtreatball").slideDown();
+})
+
+$('body').on('click','#product-info-modalrubbertuffbone',function() {
+  $("#modal-rubbertuffbone").slideDown();
+})
+
+//----------------------------------------END OF
+
+//DOG BOWLS AND Feeders
+$('body').on('click','#product-info-modalpuppypan',function() {
+  $("#modal-puppypan").slideDown();
+})
+
+$('body').on('click','#product-info-modaldeluxebowls',function() {
+  $("#modal-deluxebowls").slideDown();
+})
+
+$('body').on('click','#product-info-modalnonslipbowls',function() {
+  $("#modal-nonslipbowls").slideDown();
+})
+
+$('body').on('click','#product-info-modalstainlesssteelraised',function() {
+  $("#modal-stainlesssteelraised").slideDown();
+})
+
+$('body').on('click','#product-info-modalantiantpawprintbowl',function() {
+  $("#modal-antiantpawprintbowl").slideDown();
+})
+//-----------------------------------------END OF
+
+//DOG CRATES AND TRAVEL
+$('body').on('click','#product-info-modalhexagonplaypen',function() {
+  $("#modal-hexagonplaypen").slideDown();
+})
+
+$('body').on('click','#product-info-modalvisionclassicmocha',function() {
+  $("#modal-visionclassicmocha").slideDown();
+})
+
+$('body').on('click','#product-info-modalpetgearplushbucketseat',function() {
+  $("#modal-petgearplushbucketseat").slideDown();
+})
+//------------------------------------------END OF
+
+//PUPPY Products
+$('body').on('click','#product-info-modalfostercare',function() {
+  $("#modal-fostercare").slideDown();
+})
+//------------------------------------------END OF
+
+//VETERINARY
+$('body').on('click','#product-info-modalhitravel',function() {
+  $("#modal-hitravel").slideDown();
+})
+//-------------------------------------------END OF
+
+//Off the modal
+$('body').on('click','.close-modal',function() {
+
+  $(".modal-master").slideUp();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$("#product-info-modalhighenergy").click(function(){
+  $("#modal-highenergy").slideDown();
+
+});
+
+$("#product-info-modalhighperformance").click(function(){
+  $("#modal-highperformance").slideDown();
+
+});
+
+$("#product-info-modalhighstandardperformance").click(function(){
+  $("#modal-highstandardperformance").slideDown();
+
+});
+
+$("#product-info-modalhighstandard").click(function(){
+  $("#modal-highstandard").slideDown();
+
+});
+
+$("#product-info-modalhighstandard3020").click(function(){
+  $("#modal-highstandard3020").slideDown();
+
+});
+
+$("#product-info-modalhighstandardpuppyfood").click(function(){
+  $("#modal-highstandardpuppyfood").slideDown();
+
+});
+
+
+// WET DOG FOOD
+$("#product-info-modalpedigreechopped").click(function(){
+  $("#modal-pedigreechopped").slideDown();
+});
+
+$("#product-info-modalalpo").click(function(){
+  $("#modal-alpo").slideDown();
+});
+
+$("#product-info-modalwinnalotcannedfood").click(function(){
+  $("#modal-winnalotcannedfood").slideDown();
+});
+
+//DOG TREAT AND DOG BONE
+
+$("#product-info-modalchubroll").click(function(){
+  $("#modal-chubroll").slideDown();
+});
+
+$("#product-info-modalserranohambone").click(function(){
+  $("#modal-serranohambone").slideDown();
+});
+
+//DOG VITAMINS AND SUPPLEMENTS
+$("#product-info-modalwildalaskansalmonoil").click(function(){
+  $("#modal-wildalaskansalmonoil").slideDown();
+});
+
+$("#product-info-modalbrewerdriedyeast").click(function(){
+  $("#modal-brewerdriedyeast").slideDown();
+});
+
+$("#product-info-modalsuregrow").click(function(){
+  $("#modal-suregrow").slideDown();
+});
+
+
+
+$(".close-modal").click(function(){
+  $("#modal-highenergy").hide();
+  $(".modal-master").hide();
+});
